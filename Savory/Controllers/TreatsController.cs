c20a8a41db268eaf986a3ddbb9a1b7bc672d131c
Joseph Pearce <joseph.pearce05@gmail.com>
2020-08-14 13:03:11 -0700

    add Treat edit view, controller

--- Savory/Controllers/TreatsController.cs
+++ Savory/Controllers/TreatsController.cs
@@ -57,6 +57,20 @@ public ActionResult Edit(Treat treat)
   return RedirectToAction("Index");
 }
 
+public ActionResult Delete(int id)
+   {
+     var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+     return View(thisTreat);
+   }
+
+   [HttpPost, ActionName("Delete")]
+   public ActionResult DeleteConfirmed(int id)
+   {
+     var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+     _db.Treats.Remove(thisTreat);
+     _db.SaveChanges();
+     return RedirectToAction("Index");
+   }
 
 
   }

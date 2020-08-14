d1e6099ffc5218555930a6931ac0998d9a760444
Joseph Pearce <joseph.pearce05@gmail.com>
2020-08-14 14:40:36 -0700

    CRUD working for both Flavor & TrTreat

--- Savory/Controllers/FlavorsController.cs
+++ Savory/Controllers/FlavorsController.cs
@@ -38,6 +38,14 @@ namespace Savory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult Details(int id)
+   {
+     var thisFlavor = _db.Flavors
+         .Include(flavor => flavor.Treats)
+         .ThenInclude(join => join.Treat)
+         .FirstOrDefault(flavor => flavor.FlavorId == id);
+     return View(thisFlavor);
+   }
 
     public ActionResult Edit(int id)
     {
@@ -58,20 +66,20 @@ namespace Savory.Controllers
       return RedirectToAction("Index");
     }
 
-      public ActionResult Delete(int id)
-      {
-        var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
-        return View(thisFlavor);
-      }
+    public ActionResult Delete(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      return View(thisFlavor);
+    }
 
-      [HttpPost, ActionName("Delete")]
-      public ActionResult DeleteConfirmed(int id)
-      {
-        var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
-        _db.Flavors.Remove(thisFlavor);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-      }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
 
 
--- Savory/Models/FlavorTreat.cs
+++ Savory/Models/FlavorTreat.cs
@@ -3,9 +3,10 @@ namespace Savory.Models
   public class FlavorTreat
   {
     public int FlavorTreatId {get; set;}
-    public int TreatId {get; set;}
     public int FlavorId {get; set;}
-    public virtual Treat Treat {get; set;}
+    public int TreatId {get; set;}
     public virtual Flavor Flavor {get; set;}
+    public virtual Treat Treat {get; set;}
+    
   }
 }

eeecb48f8ec4bfc101a397793c652cf4f174ccba
Joseph Pearce <joseph.pearce05@gmail.com>
2020-08-14 14:10:03 -0700

    add Flavor Delete View, Controller

--- Savory/Controllers/FlavorsController.cs
+++ Savory/Controllers/FlavorsController.cs
@@ -58,6 +58,21 @@ namespace Savory.Controllers
       return RedirectToAction("Index");
     }
 
+      public ActionResult Delete(int id)
+      {
+        var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+        return View(thisFlavor);
+      }
+
+      [HttpPost, ActionName("Delete")]
+      public ActionResult DeleteConfirmed(int id)
+      {
+        var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+        _db.Flavors.Remove(thisFlavor);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
 
 
   }
